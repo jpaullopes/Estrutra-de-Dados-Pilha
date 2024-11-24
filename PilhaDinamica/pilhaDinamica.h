856af2b1edf75b1c9f828525bff68ca91c8e964f
Joao Paulo Lopes <jpaullopes27@gmail.com>
2024-11-23 22:17:53 -0300

    terminando projeto

--- PilhaDinamica/pilhaDinamica.h
+++ PilhaDinamica/pilhaDinamica.h
@@ -16,5 +16,21 @@ typedef struct pilha{
     No *topo;
 }Pilha;
 
+//criar pilha
+Pilha* criarPilha();
+//liberar pilha
+void liberarPilha(Pilha *pi);
+//tamanho da pilha
+int tamanhoPilha(Pilha *pi);
+//pilha vazia
+int pilhaVazia(Pilha *pi);
+//inserir na pilha
+int push(Pilha *pi, Aluno al);
+//remover da pilha
+int pop(Pilha *pi);
+//consultar elemento do topo
+int consultaTopoPilha(Pilha *pi, Aluno *al);
+//imprimir pilha
+void imprimirPilha(Pilha *pi);
 
 #endif // PILHADINAMICA_H
